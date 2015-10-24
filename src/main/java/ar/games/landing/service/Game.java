08dc17a446c1282524b3c779dcb8e4fe3473a125
nachobelfast <nachodeflores@gmail.com>
2015-10-24 01:14:37 -0500

    refactoring

--- src/main/java/ar/games/landing/service/Game.java
+++
@@ -1,5 +0,0 @@
-package ar.games.landing.service;
-
-public interface Game {
-	public void play();
-}
--- src/main/java/ar/games/landing/service/Module.java
+++
@@ -1,7 +0,0 @@
-package ar.games.landing.service;
-
-public interface Module {
-	void start(String configName);
-	float getYt();
-	void setIa(float ia);
-}
